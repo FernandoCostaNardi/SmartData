bb03516f609a50b98df94f08f0034eeedc46cbbe
fernando.nardi <fernando.nardi@senior.com.br>
2023-05-15 21:19:08 -0300

    Primeiro commit

---
+++ src/test/java/com/olbnar/smartdata/SmartdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olbnar.smartdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
